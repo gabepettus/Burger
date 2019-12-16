c9528663338eb90d32f4ddbaecf5b109a06bfdd6
gdpettus <gabe.pettus@gmail.com>
2019-12-15 19:12:31 -0700

    added jawDB env variable referance

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 const mysql = require("mysql");
-
 const PSWD = process.env.MYSQL_PSWD;
-const connection = mysql.createConnection({
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
