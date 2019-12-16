ec0e2683fc26708997defec7671f5de3fae38d2c
gdpettus <gabe.pettus@gmail.com>
2019-12-15 19:29:54 -0700

    syntax error

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql");
 const PSWD = process.env.MYSQL_PSWD;
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: PSWD,
-  database: "burgers_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: PSWD,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -23,19 +24,3 @@ connection.connect(function(err) {
 })
 
 module.exports =  connection;
-/*
-function dbConnect() {
-  // console.log("here");
-  return {
-    query(sql, args) {
-      return util.promisify( connection.query)
-        .call ( connection, sql, args);
-    },
-    close() {
-      console.log("closing");
-      return util.promisify( connection.end ).call(connection);
-    }
-  };
-};
-module.exports =  dbConnect();
-*/
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
 INSERT INTO burgers (name,isDevoured) VALUES ("Tofu and Bacon",0);
 INSERT INTO burgers (name,isDevoured) VALUES ("Tofu and smoked goat",0);
 INSERT INTO burgers (name,isDevoured) VALUES ("Tofu and goat cheese",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Tofu and turd ",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Beef and Grease",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Grease and more Grease",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Colesterol and more Grease",1);
